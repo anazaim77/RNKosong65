1f7745981739e5b0f3fc8d44660697e1d54bf7c4
Fajar <anazaim77@gmail.com>
2021-09-14 15:05:16 +0700

    refactor struktir navigation

--- src/configs/routes/AppNavigator.js
+++ src/configs/routes/AppNavigator.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
 import BottomMainTab from '../../containers/pages/MainTab/BottomMainTab';
 import screenProps from './screenProps';
 import screens from './screens';
@@ -9,13 +9,14 @@ const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName={screens.login}>
-      <Stack.Screen name={screens.mainMenu} component={BottomMainTab} />
-      {screenProps.map((el, id) => {
-        return <Stack.Screen key={id} {...el} />;
-      })}
-      {/* <Stack.Screen name="Home2" component={Home2Screen} /> */}
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={screens.login}>
+        <Stack.Screen name={screens.mainMenu} component={BottomMainTab} />
+        {screenProps.map((el, id) => {
+          return <Stack.Screen key={id} {...el} />;
+        })}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
