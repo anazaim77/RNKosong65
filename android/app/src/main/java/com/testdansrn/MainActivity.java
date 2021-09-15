8498c218aae427d93e3137287bf491ce34cc9878
Fajar <anazaim77@gmail.com>
2021-09-15 10:03:38 +0700

    change name test dans

--- android/app/src/main/java/com/testdansrn/MainActivity.java
+++ android/app/src/main/java/com/testdansrn/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnkosong65;
+package com.testdansrn;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNKosong65";
+    return "testdansrn";
   }
 
   @Override
--- ios/testdansrnTests/testdansrnTests.m
+++ ios/testdansrnTests/testdansrnTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNKosong65Tests : XCTestCase
+@interface testdansrnTests : XCTestCase
 
 @end
 
-@implementation RNKosong65Tests
+@implementation testdansrnTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
